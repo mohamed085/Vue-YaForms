008a386f9a67489ac31a67e3c89f02ef0712c46d
mohamed085 <mohamed085555@gmail.com>
2021-08-14 00:07:43 +0200

    add footer component

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <main-header></main-header>
+
+    <main-footer></main-footer>
   </div>
 </template>
 
 <script>
 import MainHeader from "../components/Main/MainHeader";
+import MainFooter from "../components/Main/MainFooter";
 export default {
 name: "Test",
-  components: {MainHeader},
+  components: {MainFooter, MainHeader},
 }
 </script>
 
