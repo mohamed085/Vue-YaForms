5da8c529c197612c0ecdf3f87491bf34830e566a
mohamed085 <mohamed085555@gmail.com>
2021-08-14 14:19:39 +0200

    add profile page

--- src/pages/Profile/ProfileIndex.vue
+++ src/pages/Profile/ProfileIndex.vue
@@ -1,9 +1,252 @@
 <template>
-  <div></div>
+  <div class="home animate__animated animate__fadeIn">
+
+    <div class="nav">
+      <profile-header
+          :avatar="user.imgSrc"
+          :name="user.name"
+          :id="user.id"
+      ></profile-header>
+    </div>
+
+    <main class="main-content en animate__animated animate__backInLeft" v-if="getLang === 'en'">
+      <div class="main-content-container container">
+        <div class="img">
+          <b-button class="avatar-btn" v-b-modal.modal-1>
+            <b-avatar class="avatar" :src=user.imgSrc></b-avatar>
+          </b-button>
+
+          <b-modal id="modal-1" title="Profile image" ok-only>
+            <div class="img-container">
+              <img class="model-img" :src=user.imgSrc>
+            </div>
+          </b-modal>
+          <h2 class="name mt-2">{{user.name}}</h2>
+        </div>
+        <div class="personalInfo p-4">
+          <h2>Personal Info</h2>
+          <div class="container">
+            <div class="personalInfo-row mt-4 mb-4">
+              <i class="fas fa-user-edit"></i>
+              <h4>Name: <span>{{user.name}}</span></h4>
+            </div>
+            <div class="personalInfo-row mt-4 mb-4">
+              <i class="fas fa-baby"></i>
+              <h4>Birth of date: <span>{{user.birthDate}}</span></h4>
+            </div>
+            <div class="personalInfo-row mt-4 mb-4">
+              <i class="far fa-flag"></i>
+              <h4>Country: <span>{{user.country}}</span></h4>
+            </div>
+            <div class="personalInfo-row mt-4 mb-4">
+              <i class="fas fa-venus-mars"></i>
+              <h4>Gender: <span>{{user.gender}}</span></h4>
+            </div>
+            <div class="personalInfo-row mt-4 mb-4">
+              <i class="fas fa-mobile-alt"></i>
+              <h4>Mobile number: <span>{{user.mobileNumber}}</span></h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <main class="main-content ar animate__animated animate__backInRight" v-if="getLang === 'ar'">
+      <div class="main-content-container container">
+        <div class="img">
+          <b-button class="avatar-btn" v-b-modal.modal-1>
+            <b-avatar class="avatar" :src=user.imgSrc></b-avatar>
+          </b-button>
+
+          <b-modal id="modal-1" title="الصورة الشخصية" ok-only>
+            <div class="img-container">
+              <img class="model-img" :src=user.imgSrc>
+            </div>
+          </b-modal>
+          <h2 class="name mt-3">{{user.name}}</h2>
+        </div>
+        <div class="personalInfo p-4">
+          <h2 class="mb-4" style="font-family: 'Cairo', sans-serif;">معلومات شخصية</h2>
+          <div class="container">
+            <div class="personalInfo-row mt-4 mb-4">
+              <i class="fas fa-user-edit"></i>
+              <h4>الاسم: <span>{{user.name}}</span></h4>
+            </div>
+            <div class="personalInfo-row mt-4 mb-4">
+              <i class="fas fa-baby"></i>
+              <h4>تاريخ الميلاد: <span>{{user.birthDate}}</span></h4>
+            </div>
+            <div class="personalInfo-row mt-4 mb-4">
+              <i class="far fa-flag"></i>
+              <h4>البلد: <span>{{user.country}}</span></h4>
+            </div>
+            <div class="personalInfo-row mt-4 mb-4">
+              <i class="fas fa-venus-mars"></i>
+              <h4>الجنس: <span>{{user.gender}}</span></h4>
+            </div>
+            <div class="personalInfo-row mt-4 mb-4">
+              <i class="fas fa-mobile-alt"></i>
+              <h4>رقم الهاتف: <span>{{user.mobileNumber}}</span></h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
+  </div>
 </template>
 
 <script>
+import ProfileHeader from "../../components/Profile/ProfileHeader";
 export default {
   name: "ProfileIndex",
+  components: {
+    ProfileHeader
+  },
+  computed: {
+    getLang() {
+      return this.$store.getters['main/getLang'];
+    }
+  },
+  data() {
+    return {
+      user: {
+        id: '161565asd',
+        imgSrc: 'https://pbs.twimg.com/media/E7yILDuVoAEOzoE?format=jpg&name=medium',
+        name: 'Mohamed Emad',
+        birthDate: '12 jun 1999',
+        country: 'Egypt',
+        gender: 'Male',
+        mobileNumber: '0100000000'
+      },
+    }
+  }
 }
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Cairo:wght@700&display=swap');
+
+main {
+  margin-top: 90px;
+}
+
+.nav {
+  position: fixed;
+  top: 0;
+  z-index: 1000;
+  width: 100%;
+}
+
+.footer {
+  position: fixed;
+  bottom: 0;
+}
+
+
+.main-content {
+  width: 100%;
+}
+
+.main-content-container {
+  display: flex;
+}
+
+.main-content-container .img {
+  padding: 10px 0;
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Roboto', sans-serif;
+}
+
+.main-content-container .img .avatar {
+  width: 250px;
+  height: 250px;
+}
+
+.main-content-container .personalInfo {
+  width: 68%;
+  margin: 10px 0;
+}
+
+.main-content-container .personalInfo h2 {
+  font-family: 'Bebas Neue', cursive;
+}
+
+.main-content-container .personalInfo .personalInfo-row {
+  display: flex;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #9d55a0;
+}
+
+.main-content-container .personalInfo .personalInfo-row i {
+  color: #9d55a0;
+  margin: auto 5px;
+  width: 5%;
+}
+
+.main-content-container .personalInfo .personalInfo-row h4 {
+  margin: auto 5px;
+}
+
+.main-content-container .personalInfo .personalInfo-row h4 span {
+  color: #5A5C6C;
+}
+
+.avatar-btn {
+  border-radius: 50%;
+  background: none;
+  border: none;
+  padding: 0;
+}
+
+.img-container {
+  height: 65vh;
+}
+
+.model-img {
+  height: 100%;
+  width: 100%;
+  border-radius: 15px;
+}
+
+.model button {
+  background-color: #9d55a0;
+}
+
+.ar {
+  direction: rtl;
+  text-align: right;
+}
+@media (max-width:1024px) {
+  .home {
+    margin-bottom: 200px;
+  }
+
+  .main-content-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .main-content-container .img,
+  .main-content-container .personalInfo {
+    width: 100%;
+  }
+
+
+  .main-content-container .img .avatar {
+    width: 200px;
+    height: 200px;
+  }
+
+  .main-content-container .personalInfo .personalInfo-row h4 {
+    display: block;
+  }
+}
+
+</style>
